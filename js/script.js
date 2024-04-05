3a37a469effe37311d2b30d3c9f8e4c4855f9991
AwareFoxy <vgstecker@gmail.com>
2024-04-05 03:37:59 +0300

    theme dark

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+document.getElementById('toggle-theme').addEventListener('click', function() {
+    document.body.classList.toggle("dark-theme");
+    let theme = "light";
+    if (document.body.classList.contains("dark-theme")) {
+        theme = "dark";
+    }
+    setCookie("theme", theme, 30);
+});
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 document.querySelectorAll('.social-link').forEach(item => {
     item.addEventListener('mouseenter', () => {
         item.style.transform = 'scale(1.05)';
