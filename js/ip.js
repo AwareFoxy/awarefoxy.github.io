51faff8543b6bd1499ffce2198fc88d434a037e6
AwareFoxy <vgstecker@gmail.com>
2024-04-05 21:54:05 +0300

    забыла

--- js/ip.js
+++ js/ip.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", () => {
     fetch('https://api.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => document.getElementById('ip').textContent = data.ip)
-        .catch(() => document.getElementById('ip').textContent = 'Ошибка загрузки');
+        .then((response) => response.json())
+        .then((data) => document.querySelector('#ip').innerText = data.ip)
+        .catch(() => document.querySelector('#ip').innerText = 'Ошибка загрузки');
 });
