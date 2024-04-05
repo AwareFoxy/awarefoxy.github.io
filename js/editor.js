9361ff42b4c24645619f2cc173c68675babd5648
AwareFoxy <vgstecker@gmail.com>
2024-04-05 03:02:26 +0300

    1984

---
+++ js/editor.js
@@ -0,0 +1,13 @@
+require.config({
+    paths: {
+        'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs',
+    }
+});
+
+require(['vs/editor/editor.main'], function () {
+    var editor = monaco.editor.create(document.getElementById('editor'), {
+        value: "// Start editing your C# code...\n",
+        language: 'csharp',
+        theme: 'vs-dark'
+    });
+});
