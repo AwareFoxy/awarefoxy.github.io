ea65c471b72365f93f7aed85cb148d42a08c2092
AwareFoxy <vgstecker@gmail.com>
2024-04-05 04:03:19 +0300

    молимся что бы все не наебнуло  🙏

--- js/script.js
+++ js/script.js
@@ -29,24 +29,9 @@ function getCookie(cookieName) {
     }
     return "";
 }
-document.querySelectorAll('.social-link').forEach(item => {
-    item.addEventListener('mouseenter', () => {
-        item.style.transform = 'scale(1.05)';
-        item.style.boxShadow = '0px 8px 24px rgba(0, 0, 0, 0.5)';
-    });
 
-    item.addEventListener('mouseleave', () => {
-        item.style.transform = 'scale(1)';
-        item.style.boxShadow = 'none';
-    });
-
-    item.addEventListener('touchstart', () => {
-        item.style.transform = 'scale(1.05)';
-        item.style.boxShadow = '0px 8px 24px rgba(0, 0, 0, 0.5)';
-    }, {passive: true});
-
-    item.addEventListener('touchend', () => {
-        item.style.transform = 'scale(1)';
-        item.style.boxShadow = 'none';
-    });
-});
+window.onload = function() {
+    if (getCookie("theme") === "dark") {
+        document.body.classList.add("dark-theme");
+    }
+};
