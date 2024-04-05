36704a2af13b9fa70406214ba52a5dbee5ddeff4
AwareFoxy <vgstecker@gmail.com>
2024-04-05 03:50:18 +0300

    update

--- js/script.js
+++ js/script.js
@@ -7,23 +7,23 @@ document.getElementById('toggle-theme').addEventListener('click', function() {
     setCookie("theme", theme, 30);
 });
 
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setCookie(cookieName, cookieValue, expiryDays) {
+    var date = new Date();
+    date.setTime(date.getTime() + (expiryDays*24*60*60*1000));
+    var expires = "expires="+ date.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
 }
 
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
+    for(var i = 0; i < cookieArray.length; i++) {
+        var c = cookieArray[i];
+        while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) === 0) {
+        if (c.indexOf(name) == 0) {
             return c.substring(name.length, c.length);
         }
     }
