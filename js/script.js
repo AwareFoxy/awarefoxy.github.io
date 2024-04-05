15ed0702423d394d71c8df01b4edbd629751d910
AwareFoxy <vgstecker@gmail.com>
2024-04-05 04:37:09 +0300

    1

--- js/script.js
+++ js/script.js
@@ -18,17 +18,29 @@ function getCookie(cookieName) {
     var name = cookieName + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var cookieArray = decodedCookie.split(';');
-    for(var i = 0; i < cookieArray.length; i++) {
-        var c = cookieArray[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
+    for(var i = 0; i <cookieArray.length; i++) {
+        var c = cookieArray[i].trim();
         if (c.indexOf(name) == 0) {
             return c.substring(name.length, c.length);
         }
     }
     return "";
 }
+
+document.getElementById('discord-copy-trigger').addEventListener('click', function() {
+    var tooltipTextElement = document.getElementById('discord-id');
+    var tooltipText = tooltipTextElement.getAttribute('data-tooltip-text');
+    var dummy = document.createElement("input");
+    document.body.appendChild(dummy);
+    dummy.value = tooltipText;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    tooltipTextElement.textContent = 'Скопировано!';
+    setTimeout(function() {
+        tooltipTextElement.textContent = 'Копировать id';
+    }, 2000);
+});
 
 window.onload = function() {
     if (getCookie("theme") === "dark") {
