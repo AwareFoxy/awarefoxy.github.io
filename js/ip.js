86945fcd15d79239da2948ed2cbe887cae777300
AwareFoxy <vgstecker@gmail.com>
2024-04-08 01:14:03 +0300

    bureaucracy

--- js/ip.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch('https://api.ipify.org?format=json')
-        .then((response) => response.json())
-        .then((data) => document.querySelector('#ip').innerText = data.ip)
-        .catch(() => document.querySelector('#ip').innerText = 'Ошибка загрузки');
-});
